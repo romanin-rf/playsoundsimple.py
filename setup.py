24996ca9e744fd90ed079ca42d914b41767b09d0
Romanin <60302782+romanin-rf@users.noreply.github.com>
2022-02-03 21:57:59 +0200

    Mini-fix for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='playsoundsimple.py',
-    version='0.1',
+    version='0.1.1',
     description='A simple library for playing sound files.',
     keywords='playsoundsimple',
     packages=setuptools.find_packages(),
